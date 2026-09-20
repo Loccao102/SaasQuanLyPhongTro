5e5d46b1fcc157917c7edd0b7ccc463ae812e448
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 02:23:47 +0700

    Register SaaS commercial domain module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { CmsModule } from "./modules/cms/cms.module.js";
+import { CommercialModule } from "./modules/commercial/commercial.module.js";
 import { HealthModule } from "./modules/health/health.module.js";
 import { IdentityModule } from "./modules/identity/identity.module.js";
 import { LeasingModule } from "./modules/leasing/leasing.module.js";
@@ -11,6 +12,7 @@ import { PropertiesModule } from "./modules/properties/properties.module.js";
     IdentityModule,
     PropertiesModule,
     LeasingModule,
+    CommercialModule,
     CmsModule
   ]
 })
