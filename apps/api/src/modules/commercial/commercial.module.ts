84e2dbc7d8044e718c65b9193afb83cc48428459
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 11:13:18 +0700

    Register internal SaaS billing scheduler endpoint

--- apps/api/src/modules/commercial/commercial.module.ts
+++ apps/api/src/modules/commercial/commercial.module.ts
@@ -1,13 +1,17 @@
 import { Module } from "@nestjs/common";
+import { InternalServiceGuard } from "../internal/internal-service.guard.js";
 import { DatabaseModule } from "../database/database.module.js";
 import { AutomationQuotaService } from "./application/automation-quota.service.js";
 import { CommercialPolicyService } from "./application/commercial-policy.service.js";
 import { SubscriptionBillingService } from "./application/subscription-billing.service.js";
 import { SubscriptionManagementService } from "./application/subscription-management.service.js";
+import { BillingInternalController } from "./billing-internal.controller.js";
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [BillingInternalController],
   providers: [
+    InternalServiceGuard,
     CommercialPolicyService,
     SubscriptionManagementService,
     SubscriptionBillingService,
