419f4bb17b646982efee8bd23963c138891aada7
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 09:36:00 +0700

    Register membership entitlement enforcement service

--- apps/api/src/modules/identity/identity.module.ts
+++ apps/api/src/modules/identity/identity.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { CommercialModule } from "../commercial/commercial.module.js";
+import { DatabaseModule } from "../database/database.module.js";
 import { AccessControlService } from "./access-control.service.js";
+import { MembershipApplicationService } from "./application/membership-application.service.js";
 
 @Module({
-  providers: [AccessControlService],
-  exports: [AccessControlService]
+  imports: [DatabaseModule, CommercialModule],
+  providers: [AccessControlService, MembershipApplicationService],
+  exports: [AccessControlService, MembershipApplicationService]
 })
 export class IdentityModule {}
