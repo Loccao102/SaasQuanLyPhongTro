87c292e75e72671b3747144eb6541159c8bfdd28
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 10:06:46 +0700

    Register internal notification worker controller

--- apps/api/src/modules/notifications/notifications.module.ts
+++ apps/api/src/modules/notifications/notifications.module.ts
@@ -5,10 +5,14 @@ import { IdentityModule } from "../identity/identity.module.js";
 import { NotificationCampaignService } from "./application/notification-campaign.service.js";
 import { NotificationOperationsService } from "./application/notification-operations.service.js";
 import { NotificationWorkerService } from "./application/notification-worker.service.js";
+import { NotificationInternalController } from "./notification-internal.controller.js";
+import { InternalWorkerGuard } from "./internal-worker.guard.js";
 
 @Module({
   imports: [DatabaseModule, IdentityModule, CommercialModule],
+  controllers: [NotificationInternalController],
   providers: [
+    InternalWorkerGuard,
     NotificationCampaignService,
     NotificationWorkerService,
     NotificationOperationsService
