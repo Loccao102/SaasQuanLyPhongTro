821679b88ca4b7f5466800772671f1d0a5e68109
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 09:39:13 +0700

    Wire commercial subscription service into CMS

--- apps/api/src/modules/cms/cms.module.ts
+++ apps/api/src/modules/cms/cms.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { CommercialModule } from "../commercial/commercial.module.js";
 import { DatabaseModule } from "../database/database.module.js";
 import { CmsController } from "./cms.controller.js";
 import { CmsPlatformGuard } from "./cms-platform.guard.js";
 import { CmsService } from "./cms.service.js";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CommercialModule],
   controllers: [CmsController],
   providers: [CmsPlatformGuard, CmsService]
 })
