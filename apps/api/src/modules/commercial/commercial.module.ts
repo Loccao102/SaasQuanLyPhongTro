dfebeb48840fb37f631e086113ace957640ced1c
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 09:38:15 +0700

    Export subscription management service

--- apps/api/src/modules/commercial/commercial.module.ts
+++ apps/api/src/modules/commercial/commercial.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { CommercialPolicyService } from "./application/commercial-policy.service.js";
+import { SubscriptionManagementService } from "./application/subscription-management.service.js";
 
 @Module({
-  providers: [CommercialPolicyService],
-  exports: [CommercialPolicyService]
+  providers: [CommercialPolicyService, SubscriptionManagementService],
+  exports: [CommercialPolicyService, SubscriptionManagementService]
 })
 export class CommercialModule {}
