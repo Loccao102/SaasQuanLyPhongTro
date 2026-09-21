203ed6635cf6b72967884c0624a8626a680c069b
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 12:36:56 +0700

    Connect CMS to billing webhook integration operations

--- apps/api/src/modules/cms/cms.module.ts
+++ apps/api/src/modules/cms/cms.module.ts
@@ -1,13 +1,19 @@
 import { Module } from "@nestjs/common";
 import { CommercialModule } from "../commercial/commercial.module.js";
 import { DatabaseModule } from "../database/database.module.js";
+import { IntegrationsModule } from "../integrations/integrations.module.js";
 import { NotificationsModule } from "../notifications/notifications.module.js";
 import { CmsController } from "./cms.controller.js";
 import { CmsPlatformGuard } from "./cms-platform.guard.js";
 import { CmsService } from "./cms.service.js";
 
 @Module({
-  imports: [DatabaseModule, CommercialModule, NotificationsModule],
+  imports: [
+    DatabaseModule,
+    CommercialModule,
+    NotificationsModule,
+    IntegrationsModule
+  ],
   controllers: [CmsController],
   providers: [CmsPlatformGuard, CmsService]
 })
