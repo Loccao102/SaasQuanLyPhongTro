97ed39f692ea69cf334937de0aebc60e18ed232b
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 09:35:57 +0700

    Register room entitlement enforcement service

--- apps/api/src/modules/properties/properties.module.ts
+++ apps/api/src/modules/properties/properties.module.ts
@@ -1,4 +1,12 @@
 import { Module } from "@nestjs/common";
+import { CommercialModule } from "../commercial/commercial.module.js";
+import { DatabaseModule } from "../database/database.module.js";
+import { IdentityModule } from "../identity/identity.module.js";
+import { RoomApplicationService } from "./application/room-application.service.js";
 
-@Module({})
+@Module({
+  imports: [DatabaseModule, IdentityModule, CommercialModule],
+  providers: [RoomApplicationService],
+  exports: [RoomApplicationService]
+})
 export class PropertiesModule {}
