03f2ecf4d03c50866cd8b747f9f5b52978fbc1db
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-21 10:02:29 +0700

    Connect notification operations to CMS

--- apps/api/src/modules/cms/cms.module.ts
+++ apps/api/src/modules/cms/cms.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { CommercialModule } from "../commercial/commercial.module.js";
 import { DatabaseModule } from "../database/database.module.js";
+import { NotificationsModule } from "../notifications/notifications.module.js";
 import { CmsController } from "./cms.controller.js";
 import { CmsPlatformGuard } from "./cms-platform.guard.js";
 import { CmsService } from "./cms.service.js";
 
 @Module({
-  imports: [DatabaseModule, CommercialModule],
+  imports: [DatabaseModule, CommercialModule, NotificationsModule],
   controllers: [CmsController],
   providers: [CmsPlatformGuard, CmsService]
 })
