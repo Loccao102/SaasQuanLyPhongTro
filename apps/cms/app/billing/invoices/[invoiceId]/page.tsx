a1cb5acf0b0c249d1ff87d3b9b6b720716c21638
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-22 09:58:01 +0700

    Add CMS invoice detail route

---
+++ apps/cms/app/billing/invoices/[invoiceId]/page.tsx
@@ -0,0 +1,10 @@
+import { InvoiceDetailClient } from "./invoice-detail-client";
+
+export default async function InvoiceDetailPage({
+  params
+}: {
+  params: Promise<{ invoiceId: string }>;
+}) {
+  const { invoiceId } = await params;
+  return <InvoiceDetailClient invoiceId={invoiceId} />;
+}
