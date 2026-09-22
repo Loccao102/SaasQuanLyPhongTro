fc9d15f6727120c9e544ad31af43bea8b28fb7d6
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-22 11:03:50 +0700

    Register CMS global search

--- apps/api/src/modules/cms/cms.module.ts
+++ apps/api/src/modules/cms/cms.module.ts
@@ -4,6 +4,8 @@ import { DatabaseModule } from "../database/database.module.js";
 import { IntegrationsModule } from "../integrations/integrations.module.js";
 import { NotificationsModule } from "../notifications/notifications.module.js";
 import { CmsController } from "./cms.controller.js";
+import { CmsGlobalSearchController } from "./cms-global-search.controller.js";
+import { CmsGlobalSearchService } from "./cms-global-search.service.js";
 import { CmsBillingDetailController } from "./cms-billing-detail.controller.js";
 import { CmsBillingDetailService } from "./cms-billing-detail.service.js";
 import { CmsOrganizationDirectoryController } from "./cms-organization-directory.controller.js";
@@ -18,7 +20,7 @@ import { CmsService } from "./cms.service.js";
     NotificationsModule,
     IntegrationsModule
   ],
-  controllers: [CmsController, CmsOrganizationDirectoryController, CmsBillingDetailController],
-  providers: [CmsPlatformGuard, CmsService, CmsOrganizationDirectoryService, CmsBillingDetailService]
+  controllers: [CmsController, CmsOrganizationDirectoryController, CmsBillingDetailController, CmsGlobalSearchController],
+  providers: [CmsPlatformGuard, CmsService, CmsOrganizationDirectoryService, CmsBillingDetailService, CmsGlobalSearchService]
 })
 export class CmsModule {}
