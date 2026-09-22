6b7062c3aac43d3e883ceed6ce91c3638f9e10ae
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-22 11:37:39 +0700

    Register tenant asset read endpoints

--- apps/api/src/modules/properties/properties.module.ts
+++ apps/api/src/modules/properties/properties.module.ts
@@ -2,11 +2,14 @@ import { Module } from "@nestjs/common";
 import { CommercialModule } from "../commercial/commercial.module.js";
 import { DatabaseModule } from "../database/database.module.js";
 import { IdentityModule } from "../identity/identity.module.js";
+import { AssetReadController } from "./application/asset-read.controller.js";
+import { AssetReadService } from "./application/asset-read.service.js";
 import { RoomApplicationService } from "./application/room-application.service.js";
 
 @Module({
   imports: [DatabaseModule, IdentityModule, CommercialModule],
-  providers: [RoomApplicationService],
-  exports: [RoomApplicationService]
+  controllers: [AssetReadController],
+  providers: [RoomApplicationService, AssetReadService],
+  exports: [RoomApplicationService, AssetReadService]
 })
 export class PropertiesModule {}
